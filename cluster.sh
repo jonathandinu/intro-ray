8582f955c3a85ad9f4e07d6e308b26348330de71
Jonathan Dinu <jondinu@gmail.com>
2022-07-01 12:44:35 -0400

    add ray cluster demo

--- cluster.sh
+++ cluster.sh
@@ -1,2 +1,8 @@
 # shell commands to launch a GCP cluster
 ray up -y config.yaml
+
+# open up dashboard
+ray dashboard config.yaml --port 8888
+
+# submit job to cluster
+ray submit config.yaml demo.py
